42c6dd1cbdbc581e3acf0596f5fc2179744d8d4d
Henry Kelderman <h.kelderman@staaltechnologies.com>
2020-07-13 00:00:56 +0200

    working with check of spec in filename

--- jobcheck.cpp
+++ jobcheck.cpp
@@ -17,22 +17,88 @@ int main(int argc, char** argv)
     vector<string> conf{split(singleline, '=')};
     if(conf[0]=="jobsdirectory")
     {
-        cout << "directory is " << conf[1];
+        cout << "directory is " << conf[1] << '\n';
     }
     if(argc<1) use_message();
     for(int i=1; i<argc;++i)
     {
         string jobfile = conf[1]+'/'+argv[i]+'/'+argv[i]+'/'+"convert.cfg";
-        cout << jobfile << '\n';
+        string paramfile = conf[1]+'/'+argv[i]+'/'+argv[i]+'/'+"convert_param.log";
+        cout << conf[1] << '\n';
+        cout << "my convert file is " << jobfile << '\n';
+        vector<string> setting, filenameelement;
+        ifstream myparamfile(paramfile);
+        string specfromfilename;
+        while(getline(myparamfile, singleline))
+        {
+            setting = split(singleline, '=');
+            if (setting[0]=="Source Name ")
+            {
+                cout << setting[1] << '\t';
+                filenameelement = split(setting[1],'_');
+                specfromfilename = filenameelement[1].erase(0,1);
+            }
+        }
         ifstream myjobfile(jobfile);
-        vector<string> setting;
+        bool mirrorx, mirrory, darkfield;
         while(getline(myjobfile, singleline))
         {
-            cout << singleline << '\n';
             setting = split(singleline, ':');
-            cout << setting[1] << '\n';
+            if (setting[0]=="mirroring at x axis (yes|no) .................. ")
+            {
+                if (setting[1]==" no")
+                    {mirrorx=false;}
+                else
+                    {mirrorx=true;}
+            }
+            if (setting[0]=="mirroring at y axis (yes|no) .................. ")
+            {
+                if (setting[1]==" no")
+                    {mirrory=false;}
+                else
+                    {mirrory=true;}
+            }
+            if (setting[0]=="exposure mode (inverted|noninverted) .......... ")
+            {
+                if (setting[1]==" 2")
+                {
+                    darkfield=false;
+                }
+                else if (setting[1]==" 0")
+                {
+                    darkfield=true;
+                }
+            }
         }
-        cout << singleline << '\n';
+        string settingconvertcfg;
+        if (darkfield)
+        {
+            cout << "DF";
+            settingconvertcfg="DF";
+        }
+        else
+        {
+            cout << "BF";
+            settingconvertcfg="BF";
+        }
+        if (mirrorx==mirrory)
+        {
+               cout << "CDWR\t";
+               settingconvertcfg+="CDWR";
+        }
+        if (mirrorx!=mirrory)
+        {
+            cout << "CDRR\t";
+            settingconvertcfg+="CDRR";
+        }
+        if (settingconvertcfg==specfromfilename)
+        {
+            cout << "Great success\n";
+        }
+        else
+        {
+            cout << "ERROR\n";
+        }
     }
 }
 
--- makefile
+++ makefile
@@ -1,6 +1,7 @@
 jobcheck:
 	g++ -std=c++17 -c stringsplit.cpp -o stringsplit.o
 	g++ -std=c++17 -Wall -Wpedantic -Werror -Wextra jobcheck.cpp stringsplit.o -o jc
+	./jc 20138
 jc:
 	g++ -std=c++17 -Wall -Wpedantic -Werror -Wextra -Wshadow jobcheck.cpp -fsanitize=address -fsanitize=undefined stringsplit.o -o jc
 	./jc hello world
